80e34b9e50e1355cbe42030997c99a0b6152b9de
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2020-03-21 20:26:33 +0100

    refined language setting; double quotes for strings

--- 02-small-programs/calculator-bonus-features3c.js
+++ 02-small-programs/calculator-bonus-features3c.js
@@ -1,9 +1,14 @@
-const rlsync = require('readline-sync');
+const rlsync = require("readline-sync");
 
-const MESSAGES = require('./calculator-MESSAGES.json');
+const MESSAGES = require("./calculator-MESSAGES.json");
 
 // Set language according to command line argument or default to english
-const LANGUAGE = process.argv[2] ? process.argv[2] : 'EN';
+let LANGUAGE;
+if (process.argv[2] && Object.keys(MESSAGES).includes(process.argv[2])) {
+  LANGUAGE = process.argv[2];
+} else {
+  LANGUAGE = "EN";
+}
 
 
 function prompt(msgkey) {
@@ -14,16 +19,16 @@ function getNumber(num) { // asks for number and validates user input
   let key;
   switch (num) {
     case 1:
-      key = 'firstNumber';
+      key = "firstNumber";
       break;
     case 2:
-      key = 'secondNumber';
+      key = "secondNumber";
   }
   prompt(key);
   let number = rlsync.question();
 
-  while (number.trimStart() === '' || Number.isNaN(Number(number))) {
-    prompt('validNumber');
+  while (number.trimStart() === "" || Number.isNaN(Number(number))) {
+    prompt("validNumber");
     number = rlsync.question();
   }
 
@@ -31,12 +36,12 @@ function getNumber(num) { // asks for number and validates user input
 }
 
 function getOperation() { //asks for operation and validates input
-  prompt('whatOperation');
-  prompt('operations');
+  prompt("whatOperation");
+  prompt("operations");
   let operation = Number(rlsync.question());
 
   while (![1, 2, 3, 4].includes(operation)) {
-    prompt('validOperation');
+    prompt("validOperation");
     operation = Number(rlsync.question());
   }
 
@@ -45,14 +50,14 @@ function getOperation() { //asks for operation and validates input
 
 // Initialize the continuation variable to valid setting
 // as stored in the messages object. Different for each language.
-let oneMoreAnswer = MESSAGES[LANGUAGE]['yesWords'][0];
+let oneMoreAnswer = MESSAGES[LANGUAGE]["yesWords"][0];
 
 
 // START
 
-prompt('greeting');
+prompt("greeting");
 
-while (MESSAGES[LANGUAGE]['yesWords'].includes(oneMoreAnswer)) {
+while (MESSAGES[LANGUAGE]["yesWords"].includes(oneMoreAnswer)) {
 
   console.clear();
 
@@ -76,10 +81,10 @@ while (MESSAGES[LANGUAGE]['yesWords'].includes(oneMoreAnswer)) {
       output = Number(number1) / Number(number2);
   }
 
-  console.log('=> ' + MESSAGES[LANGUAGE]['result'] + output + '\n');
+  console.log("=> " + MESSAGES[LANGUAGE]["result"] + output + "\n");
 
-  prompt('oneMore');
+  prompt("oneMore");
   oneMoreAnswer = rlsync.question();
 }
 
-prompt('goodbye');
+prompt("goodbye");
