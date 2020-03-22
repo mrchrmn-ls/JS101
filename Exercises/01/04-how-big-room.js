0cc93516db0ddac9dc90f2f0203367ca784e3a3e
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2020-03-23 00:09:02 +0100

    small update

--- Exercises/01/04-how-big-room.js
+++ Exercises/01/04-how-big-room.js
@@ -12,16 +12,16 @@ const UNITS = {
 let chosenUnit = rlsync.question("Square metres or square feet? Type 'm' or 'ft'\n");
 
 let length = rlsync.question("Enter the length of the room in " + UNITS[chosenUnit] + ":\n");
-length = Number(length);
 
 let width = rlsync.question("Enter the width of the room in " + UNITS[chosenUnit] + ":\n");
-width = Number(width);
 
-let area = (length * width).toFixed(2);
+let area = (Number(length) * Number(width)).toFixed(2);
+
 let convertedArea = convert(area, chosenUnit).toFixed(2);
 let convertedUnit = convertUnit(chosenUnit);
 
-console.log(`The area of the room is ${area} square ${UNITS[chosenUnit]} (${convertedArea} square ${UNITS[convertedUnit]}).`);
+console.log(`The area of the room is ${area} square ${UNITS[chosenUnit]} ` +
+              `(${convertedArea} square ${UNITS[convertedUnit]}).`);
 
 
 // FUNCTION DECLARATIONS
@@ -33,7 +33,6 @@ function convert(area, unit) {
     case "m":
       return area * SQM_TO_SQFT;
   }
-  return 0;
 }
 
 function convertUnit(unit) {
@@ -43,5 +42,4 @@ function convertUnit(unit) {
     case "m":
       return "ft";
   }
-  return 0;
 }
