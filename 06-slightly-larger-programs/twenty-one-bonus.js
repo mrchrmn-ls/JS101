69a1401eef633a2be27230d77d949ecc0f839717
Marc Hermann <59806588+mrchrmn@users.noreply.github.com>
2020-04-16 23:37:17 +0200

    implemented bonus features

--- 06-slightly-larger-programs/twenty-one-bonus.js
+++ 06-slightly-larger-programs/twenty-one-bonus.js
@@ -4,6 +4,7 @@ const rlsync = require("readline-sync");
 
 const MAX_SCORE = 21;
 const DEALER_MIN_SCORE = 17;
+const GAMES_TO_WIN = 3;
 
 const SUIT_SYMOBLS = {
   Clubs: "\u2663",
@@ -51,11 +52,6 @@ function shuffle(deck) {
 }
 
 
-function deal(deck, contestant) {
-  return contestant.cards.push(deck.pop());
-}
-
-
 function dealFirstFour(deck, contestant1, contestant2) {
   deal(deck, contestant1);
   deal(deck, contestant2);
@@ -64,40 +60,46 @@ function dealFirstFour(deck, contestant1, contestant2) {
 }
 
 
-function score(contestant) {
+function deal(deck, contestant) {
+  return contestant.cards.push(deck.pop());
+}
+
+
+function updateCardScore(contestant) {
   let result = 0;
   let aces = 0;
   contestant.cards.forEach(card => {
     result += Number(CARD_VALUES[card[1]]);
     aces += (card[1] === "A") ? 1 : 0;
   });
-  while ((result > 21) && (aces > 0)) {
+  while ((result > MAX_SCORE) && (aces > 0)) {
     result -= 10;
     aces -= 1;
   }
-  return result;
+  contestant.cardScore = result;
 }
 
 
-function displayCards(contestant1, contestant2, revealDealer) {
+function displayCards(dealer, player, revealDealer) {
   console.clear();
-  console.log("welcome to Twenty-One!\n");
-  displayRowOfCards(contestant1, revealDealer);
+  console.log("Welcome to Twenty-One!\n");
+  console.log(`We're playing best of ${GAMES_TO_WIN * 2 - 1}.\nThe current overall score is:\n`);
+  console.log(`PLAYER   ${player.matchScore} : ${dealer.matchScore}   DEALER\n`);
+  displayRowOfCards(dealer, revealDealer);
   console.log("\n");
-  displayRowOfCards(contestant2, true);
+  displayRowOfCards(player, true);
   console.log();
 }
 
 
 function displayRowOfCards(contestant, revealDealer) {
-  console.log(`${contestant.name}'s score: ${revealDealer ? score(contestant) : "Unknown."}\n`);
-
-  displayBasicLine("+-----+   ", contestant.cards.length);
-  displayBasicLine("|     |   ", contestant.cards.length);
+  displayBasicLine("   +-----+", contestant.cards.length);
+  displayBasicLine("   |     |", contestant.cards.length);
   displayValueLine(contestant, revealDealer);
   displaySuitsLine(contestant, revealDealer);
-  displayBasicLine("|     |   ", contestant.cards.length);
-  displayBasicLine("+-----+   ", contestant.cards.length);
+  displayBasicLine("   |     |", contestant.cards.length);
+  displayBasicLine("   +-----+", contestant.cards.length);
+  console.log(`${contestant.name}'s card score: ${revealDealer ? contestant.cardScore : `at least ${Number(CARD_VALUES[contestant.cards[0][1]]) + 1}`}`);
 }
 
 
@@ -114,8 +116,8 @@ function displayValueLine(contestant, revealDealer) {
   let line = "";
   for (let i = 0; i < contestant.cards.length; i += 1) {
     line += (revealDealer || (i === 0)) ?
-                  `| ${contestant.cards[i][1] === "10" ? "1 0" : ` ${contestant.cards[i][1]} `} |   ` :
-                  "|     |   ";
+                  `   | ${contestant.cards[i][1] === "10" ? "1 0" : ` ${contestant.cards[i][1]} `} |` :
+                  "   |     |";
   }
   console.log(line);
 }
@@ -125,45 +127,10 @@ function displaySuitsLine(contestant, revealDealer) {
   let line = "";
   for (let i = 0; i < contestant.cards.length; i += 1) {
     line += (revealDealer || (i === 0)) ?
-            `|  ${contestant.cards[i][0]}  |   ` :
-            "|     |   ";
+            `   |  ${contestant.cards[i][0]}  |` :
+            "   |     |";
   }
   console.log(line);
-}
-
-
-function compare(contestant1, contestant2) {
-  if (score(contestant1) === score(contestant2)) return "push";
-  return score(contestant1) > score(contestant2) ? contestant1 : contestant2;
-}
-
-
-function declareWinner(contestant) {
-  switch (contestant.name) {
-    case "Player":
-      console.log("CONGRATULATIONS! You have won!\n");
-      break;
-    case "Dealer":
-      console.log("SORRY, the house has won.\n");
-      break;
-    default:
-      console.log("Push! No one wins, you keep your money.\n");
-  }
-}
-
-
-function bust(contestant) {
-  if (score(contestant) > MAX_SCORE) {
-    switch (contestant.name) {
-      case "Player":
-        console.log("You're bust! Sorry, the house has won.\n");
-        return true;
-      case "Dealer":
-        console.log("Dealer's bust! Good for you, you have won.\n");
-        return true;
-    }
-  }
-  return false;
 }
 
 
@@ -171,6 +138,63 @@ function hitMe() {
   console.log("Hit or stay? (h/s)");
   let input = getValidAnswer(["h", "s"]);
   return input === "h";
+}
+
+
+function bust(contestant) {
+  if (contestant.cardScore > MAX_SCORE) {
+    switch (contestant.name) {
+      case "Player":
+        console.log("You bust! The house has won this game.\n");
+        return true;
+      case "Dealer":
+        console.log("Dealer busts! You have won this game.\n");
+        return true;
+    }
+  }
+  return false;
+}
+
+
+function weHaveMatchWinner(contestant1, contestant2) {
+  return (contestant1.matchScore >= GAMES_TO_WIN) ||
+         (contestant2.matchScore >= GAMES_TO_WIN);
+}
+
+
+function declareGameWinner(contestant) {
+  if (contestant === "push") {
+    console.log("Push! No one wins this game.\n");
+    return null;
+  }
+
+  contestant.matchScore += 1;
+
+  switch (contestant.name) {
+    case "Player":
+      console.log("You have won this game.\n");
+      break;
+    case "Dealer":
+      console.log("The house has won.\n");
+  }
+}
+
+
+function compare(contestant1, contestant2, scoreType) {
+  if (contestant1[scoreType] === contestant2[scoreType]) return "push";
+  return contestant1[scoreType] > contestant2[scoreType] ?
+         contestant1 : contestant2;
+}
+
+
+function declareMatchWinner(contestant) {
+  switch (contestant.name) {
+    case "Player":
+      console.log("CONGRATULATIONS!\nYou have won the match.\n");
+      break;
+    case "Dealer":
+      console.log("SORRY,\ but the house has won the match.\n");
+  }
 }
 
 
@@ -182,7 +206,6 @@ function playAgain() {
 
 
 // Player input handling
-
 
 function joinOr(arr, delimiter = ", ", word = "or") {
   if (arr.length === 2) {
@@ -218,51 +241,77 @@ function getValidAnswer(allowedAnswers) {
 
 while (true) { // play again loop
 
+  let player = {
+    name: "Player",
+    matchScore: 0
+  };
+
+  let dealer = {
+    name: "Dealer",
+    matchScore: 0
+  };
+
   while (true) { // game loop
+
+    player.cards = [];
+    dealer.cards = [];
 
     let deck = generateDeck();
     shuffle(deck);
 
-    let player = {
-      name: "Player",
-      cards: []
-    };
-
-    let dealer = {
-      name: "Dealer",
-      cards: []
-    };
-
     dealFirstFour(deck, player, dealer);
+    updateCardScore(player);
+    updateCardScore(dealer);
     displayCards(dealer, player, false);
 
-    while (score(player) < MAX_SCORE) { // player loop
+    while (player.cardScore < MAX_SCORE) { // player loop
       if (hitMe()) {
         deal(deck, player);
+        updateCardScore(player);
         displayCards(dealer, player, false);
         continue;
       }
       break;
     }
 
-    if (bust(player)) break;
+    if (bust(player)) {
+      dealer.matchScore += 1;
 
-    // rlsync.question("Press enter to reveal dealer's second card.");
+      if (weHaveMatchWinner(dealer, player)) break;
+
+      rlsync.question("Press enter to start the next round.");
+      continue;
+    }
+
 
     displayCards(dealer, player, true);
 
-    while (score(dealer) < DEALER_MIN_SCORE) { // dealer loop
-      rlsync.question("Press enter for dealer's next card.");
+    while (dealer.cardScore < DEALER_MIN_SCORE) { // dealer loop
+      rlsync.question("Press enter to reveal dealer's next card.");
       deal(deck, dealer);
+      updateCardScore(dealer);
       displayCards(dealer, player, true);
     }
 
-    if (bust(dealer)) break;
+    if (bust(dealer)) {
+      player.matchScore += 1;
 
-    declareWinner(compare(player, dealer));
+      if (weHaveMatchWinner(dealer, player)) break;
 
-    break;
+      rlsync.question("Press enter to start the next round.");
+      continue;
+    }
+
+    declareGameWinner(compare(player, dealer, "cardScore"));
+
+    if (weHaveMatchWinner(dealer, player)) break;
+
+    rlsync.question("Press enter to start the next round.");
   }
+
+  displayCards(dealer, player, true);
+
+  declareMatchWinner(compare(player, dealer, "matchScore"));
 
   if (playAgain()) continue;
 
